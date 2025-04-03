fbdaa702a8ab70cd3319d7745f4eb8e3f4b1151f
Ian Diniz de Oliveira <iandiniz653@gmail.com>
2025-04-03 01:22:24 -0300

    Aula 21 Componente App

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,17 +1,12 @@
 import './index.css'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
+import App from './App'
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el)
 root.render(
     <div>
-        <Primeiro />
-        <ComParametro 
-        titulo="Situação do Aluno" 
-        aluno="Ian Diniz!"
-        nota={10.0}/>
+        <App/>
     </div>
 )
