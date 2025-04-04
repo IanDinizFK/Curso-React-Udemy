6cd6cd6f6f96e0be10d3153f28e44042cb6251a2
Ian Diniz de Oliveira <iandiniz653@gmail.com>
2025-04-04 15:36:57 -0300

    Aula 25 - Componente Card #02
    Adicionando propriedades responsivas ao Card com Flexbox

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,24 +4,26 @@ import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from "./components/Aleatorio";
 import Card from "./components/basicos/layout/Card";
+import "./App.css"
 
 export default _ =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={1} max={255} />
-        </Card>
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Ian Diniz!"
-                nota={10.0} />
-        </Card>
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro />
-        </Card>
-        
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório">
+                <Aleatorio min={1} max={255} />
+            </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Ian Diniz!"
+                    nota={10.0} />
+            </Card>
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro />
+            </Card>
+        </div>
     </div>
